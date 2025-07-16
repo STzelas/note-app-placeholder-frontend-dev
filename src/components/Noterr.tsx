dc3a53eb8bc28370d56abd801bcd972ba8a6b96e
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-16 20:18:12 +0300

    feat: Noterr component created

---
+++ src/components/Noterr.tsx
@@ -0,0 +1,14 @@
+const Noterr = () => {
+  return (
+    <>
+      <div className="min-h-screen w-full relative dark:bg-black">
+        <div
+          className="absolute inset-0 z-0 gradient-white dark:gradient-black"
+        />
+        {/* Content/Components */}
+      </div>
+    </>
+  )
+}
+
+export default Noterr
