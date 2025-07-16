73f751ff2a1da880b982f81e15010782192f029e
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-16 21:05:34 +0300

    feat: Layout component instead of raw code added

--- src/components/Noterr.tsx
+++ src/components/Noterr.tsx
@@ -1,12 +1,9 @@
+import Layout from "./Layout";
+
 const Noterr = () => {
   return (
     <>
-      <div className="min-h-screen w-full relative dark:bg-black">
-        <div
-          className="absolute inset-0 z-0 gradient-white dark:gradient-black"
-        />
-        {/* Content/Components */}
-      </div>
+      <Layout/>
     </>
   )
 }
