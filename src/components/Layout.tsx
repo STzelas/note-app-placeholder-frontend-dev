bc214c3ce67014aa769bbb5fc08711f1d27f1b09
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-16 21:05:17 +0300

    feat: Layout for project added

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import {Outlet} from "react-router";
+import Header from "@/components/Header.tsx";
+
+const Layout = () => {
+  return (
+    <>
+      <div className="min-h-screen w-full relative dark:bg-black">
+        <div
+          className="absolute inset-0 z-0 gradient-white dark:gradient-black"
+        >
+          <Header/>
+          <div className={"container mx-auto min-h-[95vh] pt-24"}>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Layout
