9b4ec4561d0ef7a4b80c7ad737614cba1f567852
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-16 20:00:52 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen w-full relative">
+        <div
+          className="absolute inset-0 z-0 gradient-white"
+        />
+        {/* Content/Components */}
+      </div>
+    </>
+  )
+}
+
+export default App
