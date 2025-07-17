e11f39aaff60a6fa1fd76d6f5c72489a9561c628
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-17 22:28:05 +0300

    feat: button added with intent to add redirect logic

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import {useEffect} from "react";
+import {Button} from "@/components/ui/button.tsx";
 
 const HomePage = () => {
 
@@ -9,7 +10,16 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <h1 className="text-2xl font-bold text-center">Home Page</h1>
+        <h1 className="text-2xl font-bold text-center mt-50">Welcome to my app!</h1>
+        <div className={"flex items-center justify-center space-y-4 mt-5"}>
+          <Button
+            className={""}
+
+          >
+            Go to login!
+          </Button>
+        </div>
+
       </div>
     </>
   )
