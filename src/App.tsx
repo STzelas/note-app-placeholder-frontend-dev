740107eddb42d19625df016822485a1c9c1a63ea
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-17 22:17:06 +0300

    feat: login page route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router";
 import Noterr from "@/components/Noterr.tsx";
 import HomePage from "@/pages/HomePage.tsx";
+import LoginPage from "@/pages/LoginPage.tsx";
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route element={<Noterr/>}>
             <Route index element={<HomePage/>} />
+
+            <Route path="/login" element={<LoginPage/>}/>
 
           </Route>
         </Routes>
