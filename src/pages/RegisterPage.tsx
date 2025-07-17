cd4b5e3c38b84a8cd6ae99849ec6a82baf61113d
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-17 22:33:46 +0300

    feat: Register page created with register form and added

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,67 @@
+import {Label} from "@/components/ui/label.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
 const RegisterPage = () => {
   return (
     <>
+      <form
+        className="max-w-sm mx-auto p-8 space-y-4 mt-20 rounded-xl bg-white shadow-xl"
+      >
+        <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
+        <p className="mt-1 text-center text-gray-500 ">Create an account!</p>
+        <div className={"space-y-2"}>
+          <div>
+            <Label htmlFor="username" className="mb-1"></Label>
+            <Input
+              id="username"
+              autoFocus
+              placeholder={"Username"}
+            />
+          </div>
 
+          <div>
+            <Label htmlFor="email" className="mb-1"></Label>
+            <Input
+              id="email"
+              type={"email"}
+              autoFocus
+              placeholder={"Email"}
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="password" className="mb-1"></Label>
+            <Input
+              id="password"
+              type="password"
+              autoFocus
+              placeholder={"Password"}
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="confirmPassword" className="mb-1"></Label>
+            <Input
+              id="confirmPassword"
+              type="password"
+              autoFocus
+              placeholder={"Confirm Password"}
+            />
+            <a href="#" className={"text-xs text-gray-400 ms-1 hover:text-black"}>Already have an Account?</a>
+          </div>
+        </div>
+
+        <div className={"flex flex-col items-center justify-center space-y-2"}>
+          <Button className="" type="submit">
+            Create an Account
+          </Button>
+          <Button variant={"outline"} className="">
+            Back
+          </Button>
+        </div>
+
+      </form>
     </>
   )
 }
