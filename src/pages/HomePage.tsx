9718b3f963b1124f659a6c11898097509349284d
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-17 22:40:09 +0300

    feat: useNavigate and onClick event added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import {useEffect} from "react";
 import {Button} from "@/components/ui/button.tsx";
+import { useNavigate } from "react-router";
 
 const HomePage = () => {
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = "Welcome to, Noterr Project"
   }, []);
+
+  const redirect = () => {
+    navigate("/login");
+  }
 
   return (
     <>
@@ -14,7 +20,7 @@ const HomePage = () => {
         <div className={"flex items-center justify-center space-y-4 mt-5"}>
           <Button
             className={""}
-
+            onClick={redirect}
           >
             Go to login!
           </Button>
