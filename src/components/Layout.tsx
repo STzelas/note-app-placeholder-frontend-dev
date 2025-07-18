75414809c2e76e3c4be078c0964a1dd044e40fe1
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-18 21:38:36 +0300

    feat: footer added to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import {Outlet} from "react-router";
 import Header from "@/components/Header.tsx";
+import Footer from "@/components/Footer.tsx";
 
 const Layout = () => {
   return (
@@ -12,6 +13,7 @@ const Layout = () => {
           <div className={"container mx-auto min-h-[95vh] pt-24"}>
             <Outlet />
           </div>
+          <Footer/>
         </div>
       </div>
     </>
