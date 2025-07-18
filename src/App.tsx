cec88a3a928aec994bd0de40837c3fc7ca41e0cc
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-18 21:47:00 +0300

    feat: routes added ofr Notes and Todo pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Noterr from "@/components/Noterr.tsx";
 import HomePage from "@/pages/HomePage.tsx";
 import LoginPage from "@/pages/LoginPage.tsx";
 import RegisterPage from "@/pages/RegisterPage.tsx";
+import NotesPage from "./pages/NotesPage";
+import TodoPage from "@/pages/TodoPage.tsx";
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
 
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/note-app" element={<NotesPage/>}/>
+            <Route path="/todo-app" element={<TodoPage/>}/>
 
           </Route>
         </Routes>
