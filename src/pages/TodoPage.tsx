1d6ae9915cead2f90f137b7ae32509f5b0bb3447
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-21 13:40:55 +0300

    feat: Todo page code removed instead added component Todo

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,56 +1,13 @@
-import {useState} from "react";
-// import type { TodoFormProps } from "../types/todoTypes.ts"
+import Todo from "@/components/todo/Todo.tsx";
+
 
 const TodoPage = () => {
 
-  const [text, setText] = useState("");
-
-  // const inputRef = useRef<HTMLInputElement>(null);
-
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value)
-  }
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault()
-  //   if (text.trim() !== "") {
-  //     dispatch({type: "ADD", payload: text})
-  //     setText("")
-  //     inputRef.current?.focus()
-  //   }
-  // }
-
-  // const handleNoContent = () => {
-  //   if (text.length === 0)
-  //     inputRef.current?.focus()
-  // }
-
-  // useEffect(() => {
-  //   inputRef.current?.focus()
-  // }, [])
-
   return (
     <>
-      <form
-        className={"flex gap-4 mb-4 justify-center mt-4"}
-        // onSubmit={handleSubmit}
-      >
-        <input
-          className={"border p-2 rounded-lg"}
-          type="text"
-          value={text}
-          // ref={inputRef}
-          onChange={handleChange}
-          placeholder={"New Task..."}/>
-        <button
-          className={"bg-cf-dark-gray px-4 py-2 rounded text-white "}
-          type={"submit"}
-          // onClick={handleNoContent}
-        >
-          Add
-        </button>
-      </form>
+      <div>
+        <Todo/>
+      </div>
     </>
   )
 }
