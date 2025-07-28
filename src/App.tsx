a1b9132c6f9c185dbe9773d59bd333fc386da1e2
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:52:07 +0300

    refactor: todopage logic scrapped

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import HomePage from "@/pages/HomePage.tsx";
 import LoginPage from "@/pages/LoginPage.tsx";
 import RegisterPage from "@/pages/RegisterPage.tsx";
 import NotesPage from "./pages/NotesPage";
-import TodoPage from "@/pages/TodoPage.tsx";
 import { AuthProvider } from "@/context/AuthProvider.tsx";
 import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 import NotFoundPage from "@/pages/NotFoundPage.tsx";
@@ -26,7 +25,6 @@ function App() {
                 <Route path="note-app" element={<NotesPage/>}/>
 
               </Route>
-              <Route path="todo-app" element={<TodoPage/>}/>
               <Route path="*" element={<NotFoundPage />} />
             </Route>
           </Routes>
