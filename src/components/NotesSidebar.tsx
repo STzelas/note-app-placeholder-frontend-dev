50201f649b474bf4607974dba39f82ec81178d61
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-29 19:32:49 +0300

    feat: NoteSide bar type added and logic inside the html

--- src/components/NotesSidebar.tsx
+++ src/components/NotesSidebar.tsx
@@ -1,65 +1,62 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-// import { Button } from "./ui/button";
-// import { Trash2 } from "lucide-react";
-// import { ScrollArea } from "./ui/scroll-area";
-// import type {NotesSidebarProps} from "@/types/types.ts";
+import { Button } from "./ui/button";
+import { Trash2 } from "lucide-react";
+import { ScrollArea } from "./ui/scroll-area";
+import type {NotesSidebarProps} from "@/types/types.ts";
 
 
-export default function NotesSidebar() {
+export default function NotesSidebar({notes, onDeleteNote, createNewNote}: NotesSidebarProps) {
   return (
     <Card className="h-full">
       <CardHeader>
         <CardTitle>My Notes</CardTitle>
       </CardHeader>
       <CardContent>
-        {/*{notes.length === 0 ? (*/}
-        {/*  <EmptyState*/}
-        {/*    message="No notes yet"*/}
-        {/*    buttonText="Create your first note"*/}
-        {/*    onButtonClick={createNewNote}*/}
-        {/*  />*/}
-        {/*) : (*/}
-        {/*  <ScrollArea className="h-[calc(100vh-250px)]">*/}
-        {/*    <div>*/}
-        {/*      {notes.map((note) => (*/}
-        {/*        <div*/}
-        {/*          key={note.id}*/}
-        {/*          onClick={() => onSelectNote(note)}*/}
-        {/*          className={`p-3 rounded-md cursor-pointer hover:bg-accent transition-colors ${*/}
-        {/*            activeNoteId === note.id ? "bg-accent" : ""*/}
-        {/*          }`}*/}
-        {/*        >*/}
-        {/*          <div className="flex justify-between items-center">*/}
-        {/*            <div>*/}
-        {/*              <h3 className="font-medium">*/}
-        {/*                {note.title.substring(0, 30)}*/}
-        {/*                {note.title.length > 30 ? "..." : ""}*/}
-        {/*              </h3>*/}
-        {/*              <p className="text-sm text-muted-foreground">*/}
-        {/*                {note.content.substring(0, 40)}*/}
-        {/*                {note.content.length > 40 ? "..." : ""}*/}
-        {/*              </p>*/}
-        {/*              <p className="text-sm text-muted-foreground">*/}
-        {/*                {formatDate(note.createdAt)}*/}
-        {/*              </p>*/}
-        {/*            </div>*/}
-        {/*            <Button*/}
-        {/*              variant="ghost"*/}
-        {/*              size="icon"*/}
-        {/*              className="h-8 w-8 text-muted-foreground hover:text-destructive cursor-pointer"*/}
-        {/*              onClick={(e) => {*/}
-        {/*                e.stopPropagation();*/}
-        {/*                onDeleteNote(note.id);*/}
-        {/*              }}*/}
-        {/*            >*/}
-        {/*              <Trash2 className="h-4 w-4" />*/}
-        {/*            </Button>*/}
-        {/*          </div>*/}
-        {/*        </div>*/}
-        {/*      ))}*/}
-        {/*    </div>*/}
-        {/*  </ScrollArea>*/}
-        {/*)}*/}
+        {notes.length === 0 ? (
+          <div className={"flex justify-between"}>
+            <h3>No notes yet</h3>
+            <Button
+              className={""}
+              onClick={createNewNote}
+            >
+              Create a note
+            </Button>
+
+          </div>
+        ) : (
+          <ScrollArea className="h-[calc(100vh-250px)] mt-5">
+            <div>
+              {notes.map(note => (
+                <div
+                  key={note.id}
+                  className={"p-3 rounded-md cursor-pointer hover:bg-accent transition-colors"}
+                >
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <h3 className="font-medium">{note.title}</h3>
+                      <p className="text-sm text-muted-foreground">
+                        {note.content.slice(0, 50)}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                      </p>
+                    </div>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="h-8 w-8 text-muted-foreground hover:text-destructive cursor-pointer"
+
+                    >
+                      <Trash2
+                        onClick={() => onDeleteNote(note.id)}
+                        className="h-4 w-4" />
+                    </Button>
+                  </div>
+                </div>
+              ))}
+
+            </div>
+          </ScrollArea>
+        )}
       </CardContent>
     </Card>
   );
