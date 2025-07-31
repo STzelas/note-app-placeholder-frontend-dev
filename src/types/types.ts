5b182d0766c81993aa9f34da688ed205f466be67
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 13:48:39 +0300

    feat: new types for register and login added, note types removed

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,21 +9,22 @@ export const noteSchema = z.object({
 
 export type Note = z.infer<typeof noteSchema>;
 
-export type NoteEditorHandle = {
-  focusTitle: () => void;
-};
 
-export type NoteEditorProps = {
-  onSave: (note: Omit<Note, "id" | "createdAt">) => void;
-}
+export const registerSchema = z.object({
+  username: z.string().min(3, {error: "Username must be at least 3 characters"}),
+  firstname: z.string().min(3, {error: "First Name must be at least 3 characters"}),
+  lastname: z.string().min(3, {error: "Last Name must be at least 3 characters"}),
+  password: z.string()
+        .min(6, {error: "Password must be at least 6 characters"})
+        .regex(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/, {error: "Password must have at least 1 uppercase letter, 1 lowercase letter, and 1 number with no spaces."}),
+  // Password has a minimum of 6 characters, at least 1 uppercase letter, 1 lowercase letter and at least 1 number.
+})
 
-export type NoteViewProps = {
-  note: Note;
-  onEdit: () => void;
-}
+export type RegisterFields = z.infer<typeof registerSchema>
 
-export type NotesSidebarProps = {
-  notes: Note[];
-  createNewNote: () => void;
-  onDeleteNote: (id: string) => void;
-}
+export const loginSchema = z.object({
+  username: z.string({error: "Username is required"}),
+  password: z.string({error: "Password is required"}),
+})
+
+export type LoginFields = z.infer<typeof loginSchema>
