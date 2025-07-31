fb8484a1b678ae937547f49925b082a1adf378d6
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 13:47:56 +0300

    feat(LoginPage): react hook form and zod validation added

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,33 @@ import { Input } from "@/components/ui/input.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { Label } from "@/components/ui/label.tsx";
 import {useNavigate} from "react-router";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {type LoginFields, loginSchema} from "@/types/types.ts";
 
 export default function LoginPage() {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+
+  } = useForm<LoginFields>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  const onSubmit = async (data: LoginFields) => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      console.log(data)
+    } catch (error) {
+      setError("root", {
+        message: "There was a problem logging in. Check your credentials.",
+      })
+      console.error(error)
+    }
+  }
 
   const onRegisterRedirect = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
@@ -15,31 +39,39 @@ export default function LoginPage() {
     <>
       <form
         className="max-w-sm mx-auto p-8 space-y-4 mt-20 rounded-xl bg-white shadow-xl"
+        onSubmit={handleSubmit(onSubmit)}
       >
         <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
         <p className="mt-1 text-center text-gray-500 ">Login or create account</p>
         <div>
           <Label htmlFor="username" className="mb-1"></Label>
           <Input
+            {...register("username")}
             id="username"
             autoFocus
             placeholder={"Username"}
           />
+          {errors.username && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.username.message}</div>}
         </div>
 
         <div>
           <Label htmlFor="password" className="mb-1"></Label>
           <Input
+            {...register("password")}
             id="password"
             type="password"
             autoFocus
             placeholder={"Password"}
           />
+          {errors.password && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.password.message}</div>}
           <a href="#" className={"text-xs text-gray-400 ms-1 hover:text-black"}>Forgot your password?</a>
         </div>
         <div className={"flex flex-col items-center justify-center space-y-2"}>
-          <Button className="" type="submit">
-            Login
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Logging in..." : "Login"}
           </Button>
           <Button
             variant={"outline"}
