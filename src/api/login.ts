fd51ecbad20b22fc974ad7d4c90113d7614ff8f7
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-04 23:34:00 +0300

    feat: login fetch logic commented out, axios added

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,26 +1,51 @@
 import type {LoginFields, LoginResponse} from "@/types/types.ts";
+import axios, {type AxiosError} from "axios";
 
 export async function login ({username, password}: LoginFields):Promise<LoginResponse> {
-  const form = new URLSearchParams();
-  form.append("username", username);
-  form.append("password", password);
+  try {
+    const response = await axios.post<LoginResponse>(
+      `${import.meta.env.VITE_API_URL}/login`,
+      { username, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
-    {
-      method: "POST",
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: form.toString()
-    }
-  );
-  if (!res.ok) {
+    return response.data;
+  } catch (error) {
+    const err = error as AxiosError<{ detail?: string }>;
     let detail = "Login failed.";
-    try {
-      const data = await res.json()
-      if (typeof data?.detail === "string") detail = data.detail
-    } catch (err) {
-      console.error(err);
+    if (err.response?.data?.detail) {
+      detail = err.response.data.detail;
     }
     throw new Error(detail);
   }
-  return await res.json();
+
 }
+
+
+
+// const form = new URLSearchParams();
+// form.append("username", username);
+// form.append("password", password);
+//
+// const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
+//   {
+//     method: "POST",
+//     headers: new Headers({ 'Content-Type': 'application/json' }),
+//     body: form.toString()
+//   }
+// );
+// if (!res.ok) {
+//   let detail = "Login failed.";
+//   try {
+//     const data = await res.json()
+//     if (typeof data?.detail === "string") detail = data.detail
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   throw new Error(detail);
+// }
+// return await res.json();
