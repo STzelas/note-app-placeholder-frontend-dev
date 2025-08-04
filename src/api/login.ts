e9759e05c8a32d163ca022f99eb9132be9c19108
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-04 23:37:24 +0300

    chore: comments removed

--- src/api/login.ts
+++ src/api/login.ts
@@ -24,28 +24,3 @@ export async function login ({username, password}: LoginFields):Promise<LoginRes
   }
 
 }
-
-
-
-// const form = new URLSearchParams();
-// form.append("username", username);
-// form.append("password", password);
-//
-// const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
-//   {
-//     method: "POST",
-//     headers: new Headers({ 'Content-Type': 'application/json' }),
-//     body: form.toString()
-//   }
-// );
-// if (!res.ok) {
-//   let detail = "Login failed.";
-//   try {
-//     const data = await res.json()
-//     if (typeof data?.detail === "string") detail = data.detail
-//   } catch (err) {
-//     console.error(err);
-//   }
-//   throw new Error(detail);
-// }
-// return await res.json();
