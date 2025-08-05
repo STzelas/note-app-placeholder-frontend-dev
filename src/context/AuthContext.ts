082460dda9cfd0fe47c07956cebb6ad202390e98
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-05 19:26:41 +0300

    chore: removed lines

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,6 +1,4 @@
 import { createContext } from "react";
 import type {AuthContextProps} from "@/types/types.ts";
 
-
-
 export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
