ca4660d1fb574982aa4371c314b54da62b2b6d0d
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-14 16:54:14 +0300

    feat: updateNote api call added.

--- src/api/notes.ts
+++ src/api/notes.ts
@@ -25,3 +25,25 @@ export const saveNote = async ({title, content}: Omit<NoteType, "createdAt" | "u
     throw new Error(detail);
   }
 }
+
+export const updateNote = async (id: number, {title, content}: NoteType) => {
+  console.log(id, title, content);
+  try {
+    const response = await axiosInstance.put(`/notes/${id}`, {title, content },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    const err = error as AxiosError<{ detail?: string }>;
+    let detail = "Note update error.";
+    if (err.response?.data?.detail) {
+      detail = err.response.data.detail;
+      console.log(detail)
+    }
+    throw new Error(detail);
+  }
+}
