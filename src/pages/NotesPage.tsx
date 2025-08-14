77e8c54cb1d6d0cd56841af043f6220b3701d2f8
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-14 15:54:18 +0300

    feat: Notes state passed to NotesPage along with fetchNotes api

--- src/pages/NotesPage.tsx
+++ src/pages/NotesPage.tsx
@@ -1,32 +1,36 @@
 import NotesSidebar from "@/components/NotesSidebar.tsx";
 import NoteView from "@/components/NoteView.tsx";
+import {useEffect, useState} from "react";
+import type {NoteType} from "@/types/types.ts";
+import axiosInstance from "@/api/axiosInstance.ts";
 
 const NotesPage = () => {
 
+  const [notes, setNotes] = useState<NoteType[]>([]);
+  const [loading, setLoading ] = useState<boolean>(true);
 
-  // const handleAddNote = (note: Omit<Note, "id" | "createdAt">) => {
-  //   const newNote: Note = {
-  //     id: Date.now().toString(),
-  //     createdAt: Date.now(),
-  //     ...note,
-  //   };
-  //   setNotes((prev) => [newNote, ...prev]);
-  // };
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axiosInstance.get("http://localhost:8080/api/notes", {
+          withCredentials: true // ⬅ sends cookies with the request
+        });
+        setNotes(res.data);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  // const handleCreateClick = () => {
-  //   editorRef.current?.focusTitle(); // or scroll to editor, etc.
-  // };
-  //
-  // const handleDeleteNote = (id: string) => {
-  //   setNotes((prev) => prev.filter(note => note.id !== id));
-  //
-  // }
+    fetchNotes();
+  }, []);
 
   return (
     <>
       <div className="flex justify-center w-full space-x-4 mt-2">
-        <span className={"w-[25%]"}> <NotesSidebar/></span>
-        <span className={"w-[75%] h-[75%]"}><NoteView/></span>
+        <span className={"w-[25%]"}> <NotesSidebar notes={notes} loading={loading}/></span>
+        <span className={"w-[75%] h-[75%]"}><NoteView onNoteSaved={note => setNotes(prevState => [...prevState, note])}/></span>
       </div>
 
       {/*<NoteView/> */}
