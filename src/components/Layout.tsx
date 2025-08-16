9904519eca5428cfa5aa8b6b6ea4056fa621622f
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-16 12:54:17 +0300

    feat: toaster added to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router";
 import Header from "@/components/Header.tsx";
 import Footer from "@/components/Footer.tsx";
+import { Toaster } from "@/components/ui/sonner.tsx";
 
 const Layout = () => {
   return (
@@ -12,6 +13,7 @@ const Layout = () => {
           <Header/>
           <div className={"container mx-auto min-h-[95vh] pt-24"}>
             <Outlet />
+            <Toaster/>
           </div>
           <Footer/>
         </div>
