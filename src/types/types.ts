50b66a55264a3f27266b03b43d3e53ec10c02b71
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-22 12:11:34 +0300

    feat: error handling for white spaces when creating account added

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,13 +20,14 @@ export type TodoType = z.infer<typeof todoSchema>;
 
 
 export const registerSchema = z.object({
-  username: z.string().min(3, {error: "Username must be at least 3 characters"}),
-  firstname: z.string().min(3, {error: "First Name must be at least 3 characters"}),
-  lastname: z.string().min(3, {error: "Last Name must be at least 3 characters"}),
+  username: z.string().min(3, {error: "Username must be at least 3 characters"}).trim().regex(/^\S+$/, { message: "No spaces allowed" }),
+  firstname: z.string().min(3, {error: "First Name must be at least 3 characters"}).trim().regex(/^\S+$/, { message: "No spaces allowed" }),
+  lastname: z.string().min(3, {error: "Last Name must be at least 3 characters"}).trim().regex(/^\S+$/, { message: "No spaces allowed" }),
   password: z.string()
         .min(6, {error: "Password must be at least 6 characters"})
-        .regex(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/, {error: "Password must have at least 1 uppercase letter, 1 lowercase letter, and 1 number with no spaces."}),
-  // Password has a minimum of 6 characters, at least 1 uppercase letter, 1 lowercase letter and at least 1 number.
+        .regex(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/, {error: "Password must have at least 1 uppercase letter, 1 lowercase letter, and 1 number with no spaces."})
+        .trim(),
+  // Password has a minimum of 6 characters, at least 1 uppercase letter, 1 lowercase letter and at least 1 number and no spaces.
 })
 
 export type RegisterFields = z.infer<typeof registerSchema>
