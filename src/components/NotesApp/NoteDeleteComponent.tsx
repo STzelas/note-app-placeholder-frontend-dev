63d444bc2f4c6ed7d6db766ff82ecc443746d87e
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-22 12:11:40 +0300

    chore: line removed

--- src/components/NotesApp/NoteDeleteComponent.tsx
+++ src/components/NotesApp/NoteDeleteComponent.tsx
@@ -21,7 +21,6 @@ const NoteDeleteComponent = ({onNoteDelete, id}: NoteDeleteProps) => {
         <AlertDialogTrigger asChild>
           <Button
             type="button"
-
           >
             <Trash2 className="h-4 w-4 mr-2"/>
             Delete
